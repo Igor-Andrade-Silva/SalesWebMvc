f44a9d393004967a4e80a20f0f02222757c07695
Igor Andrade <igorandradesilva81@gmail.com>
2024-07-16 17:46:23 -0300

    Seeding Service

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -18,13 +18,13 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
-            Email = email;
-            BaseSalary = baseSalary;
+            Email = email;            
             BirthDate = birthDate;
+            BaseSalary = baseSalary;
             Department = department;
         }
 
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -4,15 +4,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Models;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using SalesWebMvc.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext");
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 21)); // Replace with your MySQL version
 
+
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(connectionString, serverVersion, mysqlOptions =>
-        mysqlOptions.MigrationsAssembly("SalesWebMvc"))
+        mysqlOptions.MigrationsAssembly("SalesWebMvc"))    
 );
 
 
@@ -20,15 +22,20 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<SeedingService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -40,6 +47,16 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var seedingService = services.GetRequiredService<SeedingService>();
+        seedingService.Seed();
+    }
+}
 
 app.Run();
 
