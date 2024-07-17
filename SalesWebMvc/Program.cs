24e2aba686f8f14dc5cc06da357bfee4a6cc10d6
Igor Andrade <igorandradesilva81@gmail.com>
2024-07-17 10:19:36 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 var app = builder.Build();
 
---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,19 @@
+﻿using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
