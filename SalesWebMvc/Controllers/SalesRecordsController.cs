dfca84ada0d2604a74a73f18fc983b47ace3fdae
Igor Andrade <igorandradesilva81@gmail.com>
2024-07-17 16:52:47 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSeach()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
