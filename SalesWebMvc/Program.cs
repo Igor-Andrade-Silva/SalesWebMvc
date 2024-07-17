293fd66c7b42f18045a958dab2ad498dc3c5a494
Igor Andrade <igorandradesilva81@gmail.com>
2024-07-17 18:33:36 -0300

    Implementing simple search

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
 
 var app = builder.Build();
 
